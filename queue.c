08ee36d1ec98e25362353dcd666c3debb8cce599
Dan <til61@pitt.edu>
2021-10-14 17:29:19 -0400

    passed 06, still failing mallocs

--- queue.c
+++ queue.c
@@ -78,7 +78,7 @@ bool q_insert_head(queue_t *q, char *s)
     }
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
-    char * news = malloc(sizeof(s));
+    char * news = malloc(strlen(s)+1);
     if(news == NULL){
       return false;
     }
@@ -113,7 +113,7 @@ bool q_insert_tail(queue_t *q, char *s)
     if(newt == NULL){
       return false;
     }
-    char * news = malloc(sizeof(s));
+    char * news = malloc(strlen(s)+1);
     if(news == NULL){
       return false;
     }
@@ -151,7 +151,8 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
       return false;
     }
     temp = q->head;
-    strcpy(sp, temp->value);
+    strncpy(sp, temp->value, bufsize-1);
+    sp[bufsize-1] = '\0';
     q->head = q->head->next;
     free(temp->value);
     free(temp);
@@ -167,6 +168,9 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
+    if(q == NULL || q->head == NULL){
+      return 0;
+    }
     return q->size;
 }
 
@@ -180,12 +184,24 @@ int q_size(queue_t *q)
 void q_reverse(queue_t *q)
 {
     /* You need to write the code for this function */
-    if(q == NULL || q->head == NULL){
+     if(q == NULL || q->head == NULL){
       return;
     }
     if(q->head == q->tail){
       return;
     }
-    
+
+    list_ele_t * prev = q->head;
+    q->head = q->head->next;
+    list_ele_t * curr = q->head;
+    prev->next = NULL;
+    q->tail = prev;
+    while(q->head != NULL){
+      q->head = q->head->next;
+      curr->next = prev;
+      prev = curr;
+      curr = q->head;
+    }
+    q->head = prev;
 }
 
